d7cb69dbf483e7d4a5f38242035b7b2b589d3b3a
Stefan Vulpe <svulpe25@gmail.com>
2023-05-03 18:54:19 +0300

    added searchbar and checkAuth function template

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -11,6 +11,7 @@ const { sign, verify } = jwt;
 export const register = async (req, res) => {
   const body = await getReqData(req);
   const user = JSON.parse(body);
+
   const { error } = Joi.object({
     firstName: Joi.string().pattern(new RegExp('^[A-Z][a-z]+$')),
     lastName: Joi.string().pattern(new RegExp('^[A-Z][a-z]+$')),
@@ -31,12 +32,34 @@ export const register = async (req, res) => {
     const authToken = sign(result.dataValues, process.env.ACCESS_SECRET_KEY, {
       expiresIn: '7d',
     });
-    const secret = await new Tokens().secret();
-    const csrfToken = new Tokens().create(secret);
+    const csrfToken = new Tokens().create(process.env.CSRF_SECRET_KEY);
     res.writeHead(201, { 'Content-type': 'application/json' });
     res.end(JSON.stringify({ error: false, authToken, csrfToken }));
   } catch (err) {
     res.writeHead(400, { 'Content-type': 'application/json' });
     res.end(JSON.stringify({ error: true, message: err.message }));
+  }
+};
+
+export const checkAuth = (req, res, next) => {
+  const authToken = req.headers['authorization'].split(' ')[1];
+  const csrfToken = req.headers['csrftoken'];
+
+  if (!authToken || !csrfToken) {
+    res.writeHead(401, { 'Content-type': 'application/json' });
+    return res.end(
+      JSON.stringify({ error: true, message: 'Missing authorization tokens.' })
+    );
+  }
+
+  try {
+    const payload = verify(authToken, process.env.ACCESS_SECRET_KEY);
+    /* Check to see if the csrf token matches the one stored in the sessions table (search by token, not user id) */
+    /* verifyCsrf(csrfToken, payload.id) */
+    req.userId = payload.id;
+    next();
+  } catch (err) {
+    res.writeHead(401, { 'Content-type': 'application/json' });
+    return res.end(JSON.stringify({ error: true, message: err.message }));
   }
 };
