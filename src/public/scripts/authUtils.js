6a16c0b7047e4213c476926ac3e01e7750b2a074
Octavian Gavril <100153009+octaviangavril@users.noreply.github.com>
2023-05-15 00:09:56 +0300

    form validation for login form and for sign up form

--- src/public/scripts/authUtils.js
+++ src/public/scripts/authUtils.js
@@ -1,4 +1,26 @@
+function showError(inputId,errorElement, errorMessage){
+  document.querySelector("." + errorElement).classList.add("display-error");
+  document.querySelector("." + errorElement).innerHTML = errorMessage;
+  document.querySelector("#" + inputId).parentElement.classList.add("input-error");
+  document.querySelector("#" + inputId).parentElement.classList.remove("success");
+}
+
+function setSuccess(inputId){
+  document.querySelector("#" + inputId).parentElement.classList.add("success");
+  document.querySelector("#" + inputId).parentElement.classList.remove("input-error");
+
+}
+
+function clearError(){
+  let errors = document.querySelectorAll(".error");
+  for(let error of errors){
+    error.classList.remove("display-error");
+  }
+}
+
 const submitLoginForm = async event => {
+  clearError();
+  
   event.preventDefault();
   // Send ajax request to server using fetch API
   const response = await fetch('/login', {
@@ -15,7 +37,16 @@ const submitLoginForm = async event => {
   const result = await response.json();
 
   if (result.error) {
-    alert('email or password wrong!');
+    if(result.message.includes('email')){
+      showError("email","email-error","Adresa de email gresita!");
+    }else{
+      setSuccess("email");
+    }
+    if(result.message.includes('password')){
+      showError("password","password-error","Parola gresita!");
+    }else{
+      setSuccess("password");
+    }
   } else {
     event.target.reset();
     localStorage.setItem('csrfToken', JSON.stringify(result.csrfToken));
@@ -24,6 +55,7 @@ const submitLoginForm = async event => {
 };
 
 const submitSignupForm = async event => {
+  clearError();
   event.preventDefault();
   let first_name = event.target.elements['first_name'].value;
   let last_name = event.target.elements['last_name'].value;
@@ -49,15 +81,30 @@ const submitSignupForm = async event => {
       photo: '',
     }),
   });
-
+  
   const result = await response.json();
-
+console.log(result.message);
   if (result.error) {
-    alert(result.message);
-    //handleErrors(result.message)
-    // if (message.contains("email")) {
-
-    //}
+    if(result.message.includes('email')){
+      showError("email","email-error","Adresa de email gresita!");
+    }else{
+      setSuccess("email");
+    }
+    if(result.message.includes('password')){
+      showError("password","password-error","Parola gresita!");
+    }else{
+      setSuccess("password");
+    }
+    if(result.message.includes('firstName')){
+      showError("first_name","first_name-error","Prenume invalid!");
+    }else{
+      setSuccess("first_name");
+    }
+    if(result.message.includes('lastName')){
+      showError("last_name","last_name-error","Nume invalid!");
+    }else{
+      setSuccess("last_name");
+    }
   } else {
     event.target.reset();
     localStorage.setItem('csrfToken', JSON.stringify(result.csrfToken));
